5b624bd3b726590e17708699f7deb4dd82fa0734
1mtrue <neverteaser@gmial.com>
2021-04-05 23:53:50 +0800

    feat(fe):监控模块CPU图表

--- web/src/components/monitor/Monitor.js
+++ web/src/components/monitor/Monitor.js
@@ -6,7 +6,7 @@ import {Col, Descriptions, Row, Table} from "antd"
 import {wsServer} from "../../common/env";
 import {getToken} from "../../utils/utils";
 import {message} from "antd/es";
-
+import moment from 'moment';
 import qs from "qs";
 
 
@@ -17,10 +17,12 @@ class StatusMonitor extends Component {
 
     state = {
         webSocket: undefined,
-        baseInfo: {},
-        memoryInfo: [{}],
-        dockerInfo: {},
-        netWorkInfo: {}
+        memoryInfo: [],
+        netWorkInfo: {},
+        totalRx: "",
+        totalTx: "",
+        memory: {},
+        cpuInfo: []
 
     }
 
@@ -31,10 +33,11 @@ class StatusMonitor extends Component {
     }
 
     componentDidMount() {
+        const dataLength = 10
+
         let param = {
             'X-Auth-Token': getToken()
         }
-        console.log(wsServer + '/monitor/' + this.state.id + "?" + qs.stringify(param))
         let webSocket = new WebSocket(wsServer + '/monitor/' + this.state.id + "?" + qs.stringify(param));
         this.setState({
             webSocket: webSocket
@@ -43,10 +46,11 @@ class StatusMonitor extends Component {
         webSocket.onopen = (e => {
             pingInterval = setInterval(() => {
                 webSocket.send("status")
-            }, 5000);
+            }, 1000);
         });
 
         webSocket.onerror = (e) => {
+            console.log("err", e)
             message.error("Failed to connect to server.");
         }
         webSocket.onclose = (e) => {
@@ -54,23 +58,46 @@ class StatusMonitor extends Component {
                 clearInterval(pingInterval);
             }
         }
+        let CpuInfo = []
+
         webSocket.onmessage = (e) => {
             let data = JSON.parse(e.data)
-            console.log(data)
-            let CpuInfo = []
-            if (this.state.memoryInfo.length < 10) {
-                CpuInfo=this.state.memoryInfo
-                CpuInfo.push(data["base_info"])
+            let baseInfo = data["base_info"]
+            let datetime = new Date(data["datetime"])
+            let datetimeStr = moment(datetime).format("YYYY-MM-DD HH:mm:ss")
+            let singleCpuInfo = {
+                "sys": baseInfo['sys_use'],
+                "user": baseInfo["user_use"],
+                "datetime": datetimeStr
             }
+            singleCpuInfo["total"] = singleCpuInfo["sys"] + singleCpuInfo["user"]
+
+            if (this.state.memoryInfo.length < 10) {
+                CpuInfo = this.state.memoryInfo
+                CpuInfo.push(singleCpuInfo)
+
+            } else {
+                CpuInfo = this.state.memoryInfo.slice(1, 10)
+                CpuInfo.push(singleCpuInfo)
+                this.setState({memoryInfo: CpuInfo})
+            }
+            let memory = {
+                "free": baseInfo["free"],
+                "used": baseInfo["used"],
+                "cache": baseInfo["cache"]
+            }
+
             this.setState({
                 baseInfo: data["base_info"],
-                memoryInfo: CpuInfo,
+                cpuInfo: CpuInfo,
+                memory: memory,
                 dockerInfo: data["docker"],
                 netWorkInfo: data['net_work'],
                 upTime: data['base_info']['uptime'],
-                onlineUser: data['base_info']["online_user"]
+                onlineUser: data['base_info']["online_user"],
+                totalRx: data['net_work']["rx_total"],
+                totalTx: data['net_work']["tx_total"]
             })
-            console.log(this.state)
         }
     }
 
@@ -82,8 +109,42 @@ class StatusMonitor extends Component {
         }
     }
 
+    parseMemory() {
+        let ret = []
+
+        let memory = this.state.memory
+        Object.keys(memory).forEach(function (key) {
+            ret.push({value: memory[key], name: key})
+        })
+        return ret
+    }
+
+    parseCpuTime() {
+        let ret = []
+        this.state.cpuInfo.forEach(function (item) {
+            ret.push(item['datetime'])
+        })
+        return ret
+    }
+
+    parseCpuData() {
+        let ret = {
+            user: [],
+            sys: [],
+            total: [],
+
+        }
+
+        this.state.cpuInfo.forEach(function (item) {
+            ret.sys.push(item['sys'])
+            ret.user.push(item['user'])
+            ret.total.push(item['user'] + item['sys'])
+        })
+        return ret
+    }
 
     render() {
+        let cpuData = this.parseCpuData()
         let baseOption = {
             title: {
                 text: 'CPU使用率'
@@ -108,7 +169,7 @@ class StatusMonitor extends Component {
             xAxis: {
                 type: 'category',
                 boundaryGap: false,
-                data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+                data: this.parseCpuTime()
             },
             yAxis: {
                 type: 'value',
@@ -119,19 +180,19 @@ class StatusMonitor extends Component {
                     name: '用户',
                     type: 'line',
                     stack: '用户',
-                    data: [20, 32, 1, 34, 9, 23, 21]
+                    data: cpuData["user"]
                 },
                 {
                     name: '系统',
                     type: 'line',
                     stack: '系统',
-                    data: [40, 10, 20, 40, 30, 33, 32]
+                    data: cpuData["sys"]
                 },
                 {
                     name: '总量',
                     type: 'line',
                     stack: '总量',
-                    data: [60, 32, 21, 74, 39, 56, 53]
+                    data: cpuData["total"]
                 }
 
             ]
@@ -153,11 +214,7 @@ class StatusMonitor extends Component {
                     name: '内存分布',
                     type: 'pie',
                     radius: '50%',
-                    data: [
-                        {value: 1048, name: '用户'},
-                        {value: 735, name: '系统'},
-                        {value: 580, name: '空闲'},
-                    ],
+                    data: this.parseMemory(),
                     emphasis: {
                         itemStyle: {
                             shadowBlur: 10,
@@ -183,7 +240,7 @@ class StatusMonitor extends Component {
             },
             series: [
                 {
-                    name: '内存分布',
+                    name: "流量分布",
                     type: 'pie',
                     radius: '50%',
                     data: [
@@ -223,78 +280,6 @@ class StatusMonitor extends Component {
                 key: 'mem_usage_limit',
             }
         ];
-        const containerData = [
-            {
-                "container_id": "3d651ab404cb",
-                "container_name": "next-terminal",
-                "cpu_pre": "0.10%",
-                "mem_usage_limit": "95.74 MiB / 7.638 GiB"
-            }, {
-                "container_id": "177e2e15be02",
-                "container_name": "k8s_kube-scheduler_kube-scheduler-k8s-master_kube-system_1550c620c1fcf6b52e24352a2f6d899e_9",
-                "cpu_pre": "0.26%",
-                "mem_usage_limit": "14.89 MiB / 7.638 GiB"
-            }, {
-                "container_id": "70980e93908f",
-                "container_name": "k8s_etcd_etcd-k8s-master_kube-system_d40f2c29b74a0cb2ecde9864ba0284d4_8",
-                "cpu_pre": "1.12%",
-                "mem_usage_limit": "56.11 MiB / 7.638 GiB"
-            }, {
-                "container_id": "bebeeffa0de2",
-                "container_name": "k8s_kube-controller-manager_kube-controller-manager-k8s-master_kube-system_06f86d57119ab927854bae68a140a585_8",
-                "cpu_pre": "0.75%",
-                "mem_usage_limit": "40.05 MiB / 7.638 GiB"
-            }, {
-                "container_id": "26b32088e815",
-                "container_name": "k8s_kube-apiserver_kube-apiserver-k8s-master_kube-system_c6fd5c60cfb20bd2a706d63360289fa3_8",
-                "cpu_pre": "2.34%",
-                "mem_usage_limit": "278.9 MiB / 7.638 GiB"
-            }, {
-                "container_id": "754b350e370c",
-                "container_name": "k8s_kube-proxy_kube-proxy-tq52n_kube-system_f16571eb-741b-43cf-ac5f-0ca4a4b8f75e_7",
-                "cpu_pre": "0.03%",
-                "mem_usage_limit": "12.74 MiB / 7.638 GiB"
-            }, {
-                "container_id": "8159d31df5a0",
-                "container_name": "k8s_POD_kube-apiserver-k8s-master_kube-system_c6fd5c60cfb20bd2a706d63360289fa3_9",
-                "cpu_pre": "0.00%",
-                "mem_usage_limit": "44 KiB / 7.638 GiB"
-            }, {
-                "container_id": "09f558fe1fa2",
-                "container_name": "k8s_POD_etcd-k8s-master_kube-system_d40f2c29b74a0cb2ecde9864ba0284d4_9",
-                "cpu_pre": "0.00%",
-                "mem_usage_limit": "36 KiB / 7.638 GiB"
-            }, {
-                "container_id": "bb89e0ddbfb8",
-                "container_name": "k8s_POD_kube-controller-manager-k8s-master_kube-system_06f86d57119ab927854bae68a140a585_10",
-                "cpu_pre": "0.00%",
-                "mem_usage_limit": "40 KiB / 7.638 GiB"
-            }, {
-                "container_id": "4f4a33e5322e",
-                "container_name": "k8s_POD_kube-proxy-tq52n_kube-system_f16571eb-741b-43cf-ac5f-0ca4a4b8f75e_7",
-                "cpu_pre": "0.00%",
-                "mem_usage_limit": "40 KiB / 7.638 GiB"
-            }, {
-                "container_id": "2344287de2c9",
-                "container_name": "k8s_POD_kube-scheduler-k8s-master_kube-system_1550c620c1fcf6b52e24352a2f6d899e_9",
-                "cpu_pre": "0.00%",
-                "mem_usage_limit": "40 KiB / 7.638 GiB"
-            }, {
-                "container_id": "e6e10aa53add",
-                "container_name": "github-runner",
-                "cpu_pre": "0.00%",
-                "mem_usage_limit": "14.3 MiB / 7.638 GiB"
-            }, {
-                "container_id": "723e477c7385",
-                "container_name": "teaser_codimd_1",
-                "cpu_pre": "0.02%",
-                "mem_usage_limit": "80.58 MiB / 7.638 GiB"
-            }, {
-                "container_id": "d27b78c5ff4b",
-                "container_name": "teaser_database_1",
-                "cpu_pre": "0.00%",
-                "mem_usage_limit": "2.734 MiB / 7.638 GiB"
-            }]
 
         return (
             <>
@@ -304,6 +289,8 @@ class StatusMonitor extends Component {
                             <Descriptions title="系统信息">
                                 <Descriptions.Item label="开机时长">{this.state.upTime}</Descriptions.Item>
                                 <Descriptions.Item label="在线用户">{this.state.onlineUser}</Descriptions.Item>
+                                <Descriptions.Item label="发送">{this.state.totalTx}</Descriptions.Item>
+                                <Descriptions.Item label="接收">{this.state.totalRx}</Descriptions.Item>
 
                             </Descriptions>
                         </Col>
@@ -332,7 +319,7 @@ class StatusMonitor extends Component {
                         <Col span={20}>
                             <div className="Status">
                                 <ReactEcharts
-                                    style={{height: 300, width: 1000, padding: 10}}
+                                    style={{height: 300, width: 1100, padding: 10}}
                                     notMerge={true}
                                     lazyUpdate={true}
                                     option={baseOption}/>
@@ -342,7 +329,7 @@ class StatusMonitor extends Component {
                     </Row>
 
                     <Table columns={containerColumns}
-                           dataSource={containerData}
+                           dataSource={this.state.dockerInfo}
                            pagination={false}
                            style={{padding: 18}}/>
                 </Content>
